82b0021a7b4bb6a79733567d1c36fb7af8e65f9e
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-03-13 00:15:36 +0200

    fix

--- src/components/PaginatedItems/PaginatedItems.jsx
+++ src/components/PaginatedItems/PaginatedItems.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+//import { useState, useEffect } from 'react';
 
 function PaginatedItems({ totalPages, setCurrentPage }) {
   // We start with an empty list of items.
