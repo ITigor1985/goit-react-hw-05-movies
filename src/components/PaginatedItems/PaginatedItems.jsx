c9c0579d20ad4ceac58cc9492cef1af05ac74e43
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-03-18 15:49:20 +0200

    fix

--- src/components/PaginatedItems/PaginatedItems.jsx
+++ src/components/PaginatedItems/PaginatedItems.jsx
@@ -1,30 +1,57 @@
-//import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
-function PaginatedItems({ totalPages, setCurrentPage, currentPage }) {
+function PaginatedItems({ totalPages, setCurrentPage }) {
   // We start with an empty list of items.
+
+  const [prevPage, setPrevPage] = useState(0);
   const numberOfPages = [];
   for (let i = 1; i <= totalPages; i++) {
     numberOfPages.push(i);
   }
 
+  useEffect(() => {
+    if (prevPage === 0) {
+      return;
+    }
+    //console.log(prevPage);
+    prevPage.style.border = '1px solid black';
+  }, [prevPage]);
+
   let sliceStartArray = [];
-  //let sliceEndArray = [];
+  let sliceEndArray = [];
+  let sumArray = [];
   if (numberOfPages.length > 5) {
     sliceStartArray = numberOfPages.slice(0, 5);
   }
+  if (numberOfPages.length > 10) {
+    sliceEndArray = numberOfPages.slice(-5);
+  }
 
+  sumArray = sliceStartArray.concat('...', sliceEndArray);
+
+  console.log(sliceEndArray);
   const handlePageChange = e => {
-    console.log(e.target.value);
     let target = e.target;
-    target.style.borderColor = 'green';
+    setPrevPage(target);
+    if (prevPage === 0) {
+      return;
+    } else {
+      prevPage.style.border = 'none';
+    }
+
     setCurrentPage(Number(e.target.value));
   };
   return (
     <>
       <ul style={{ display: 'flex', justifyContent: 'center' }}>
-        {sliceStartArray.map((page, index) => (
+        {sumArray.map((page, index) => (
           <li key={index}>
-            <button onClick={handlePageChange} type="button" value={page}>
+            <button
+              style={{ border: 'none' }}
+              onClick={handlePageChange}
+              type="button"
+              value={page}
+            >
               {page}
             </button>
           </li>
